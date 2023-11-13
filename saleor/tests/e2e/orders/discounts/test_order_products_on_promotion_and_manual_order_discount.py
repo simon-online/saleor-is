28d1a00b9ec518d19f884c5d9662b9dbfb2c94f8
Renata <renata.gajzlerowicz@gmail.com>
2023-11-13 10:02:48 +0100

    prepare_shop_with_test_refactor

--- saleor/tests/e2e/orders/discounts/test_order_products_on_promotion_and_manual_order_discount.py
+++ saleor/tests/e2e/orders/discounts/test_order_products_on_promotion_and_manual_order_discount.py
@@ -23,6 +23,8 @@ def test_order_products_on_promotion_and_manual_order_discount_CORE_2108(
     permission_manage_product_types_and_attributes,
     permission_manage_discounts,
     permission_manage_orders,
+    permission_manage_taxes,
+    permission_manage_settings,
 ):
     permissions = [
         permission_manage_products,
@@ -31,15 +33,17 @@ def test_order_products_on_promotion_and_manual_order_discount_CORE_2108(
         permission_manage_product_types_and_attributes,
         permission_manage_discounts,
         permission_manage_orders,
+        permission_manage_taxes,
+        permission_manage_settings,
     ]
     assign_permissions(e2e_staff_api_client, permissions)
 
-    (
-        result_warehouse_id,
-        result_channel_id,
-        _,
-        result_shipping_method_id,
-    ) = prepare_shop(e2e_staff_api_client)
+    shop_data = prepare_shop(
+        e2e_staff_api_client,
+    )
+    channel_id = shop_data["channel_id"]
+    warehouse_id = shop_data["warehouse_id"]
+    shipping_method_id = shop_data["shipping_method_id"]
 
     (
         product_id,
@@ -47,8 +51,8 @@ def test_order_products_on_promotion_and_manual_order_discount_CORE_2108(
         product_variant_price,
     ) = prepare_product(
         e2e_staff_api_client,
-        result_warehouse_id,
-        result_channel_id,
+        warehouse_id,
+        channel_id,
         variant_price=20,
     )
 
@@ -69,18 +73,18 @@ def test_order_products_on_promotion_and_manual_order_discount_CORE_2108(
         discount_type,
         discount_value,
         promotion_rule_name,
-        result_channel_id,
+        channel_id,
     )
     product_predicate = promotion_rule["cataloguePredicate"]["productPredicate"]["ids"]
-    assert promotion_rule["channels"][0]["id"] == result_channel_id
+    assert promotion_rule["channels"][0]["id"] == channel_id
     assert product_predicate[0] == product_id
 
     # Step 1 - Create a draft order for a product with fixed promotion
     input = {
-        "channelId": result_channel_id,
+        "channelId": channel_id,
         "billingAddress": DEFAULT_ADDRESS,
         "shippingAddress": DEFAULT_ADDRESS,
-        "shippingMethod": result_shipping_method_id,
+        "shippingMethod": shipping_method_id,
     }
     data = draft_order_create(e2e_staff_api_client, input)
     order_id = data["order"]["id"]
@@ -125,7 +129,7 @@ def test_order_products_on_promotion_and_manual_order_discount_CORE_2108(
     assert discount_value == 2
 
     # Step 4 - Add a shipping method to the order
-    input = {"shippingMethod": result_shipping_method_id}
+    input = {"shippingMethod": shipping_method_id}
     draft_update = draft_order_update(e2e_staff_api_client, order_id, input)
     order_shipping_id = draft_update["order"]["deliveryMethod"]["id"]
     shipping_price = draft_update["order"]["shippingPrice"]["gross"]["amount"]
